f6a2a8d2506a07e34e5bc0680789aa6992ef26ca
Kees van Lierop <keesvanlierop@hotmail.com>
2020-06-17 20:08:10 +0200

    GraphQL directive

---
+++ src/directive.ts
@@ -0,0 +1,50 @@
+import {
+	GraphQLArgument,
+	GraphQLInputField,
+	GraphQLSchema,
+	GraphQLDirective,
+	DirectiveLocation,
+	GraphQLString,
+} from 'graphql'
+import { GraphQLAny } from 'graphql-any-type'
+import { SchemaDirectiveVisitor } from 'graphql-tools'
+
+import { ValidationType } from './validator-scalar'
+
+export default class RequiresAuthentication extends SchemaDirectiveVisitor {
+	public visitArgumentDefinition(argument: GraphQLArgument) {
+		argument.type = new ValidationType(
+			argument.type,
+			argument.name,
+			this.args,
+		)
+	}
+
+	public visitInputFieldDefinition(field: GraphQLInputField) {
+		field.type = new ValidationType(
+			field.type,
+			field.name,
+			this.args,
+		)
+	}
+
+	public static getDirectiveDeclaration(
+		directiveName: string,
+	): GraphQLDirective {
+		return new GraphQLDirective({
+			name: directiveName,
+			locations: [
+				DirectiveLocation.INPUT_FIELD_DEFINITION,
+				DirectiveLocation.ARGUMENT_DEFINITION,
+			],
+			args: {
+				type: {
+					type: GraphQLString,
+				},
+				options: {
+					type: GraphQLAny,
+				},
+			},
+		})
+	}
+}
---
+++ src/validator-scalar.ts
@@ -0,0 +1,71 @@
+import { GraphQLScalarType, ValueNode } from 'graphql'
+import { UserInputError } from 'apollo-server-errors'
+import validator from 'validator'
+import { Maybe } from 'graphql/jsutils/Maybe'
+
+export class ValidationType extends GraphQLScalarType {
+	private validate(value: any) {
+		const { type, ...args } = this.args
+		// @ts-ignore
+		const isValid = validator[this.args.type](value, args)
+
+		if (!isValid) {
+			throw new UserInputError(
+				`Wrong value "${value}" for field ${this.field}`,
+			)
+		}
+	}
+
+	public constructor(
+		scalarType: any,
+		private field: string,
+		private args: Record<string, any>,
+	) {
+		super({
+			name: `Validator${scalarType}`,
+			description: 'Scalar type wrapper for input validation',
+
+			/**
+			 * Server -> Client
+			 */
+			serialize(value) {
+				return scalarType.serialize(value)
+			},
+
+			/**
+			 * Client (Variable) -> Server
+			 */
+			parseValue: (value) => {
+				const parsedValue = scalarType.parseValue(value)
+
+				this.validate(parsedValue)
+
+				return parsedValue
+			},
+
+			/**
+			 * Client (Param) -> Server
+			 */
+			parseLiteral: (
+				valueNode: ValueNode,
+				variables?: Maybe<{ [key: string]: any }>,
+			) => {
+				const parsedValue = scalarType.parseLiteral(
+					valueNode,
+					variables,
+				)
+
+				this.validate(parsedValue)
+
+				return parsedValue
+			},
+		})
+
+		// Wrap scalar types directly
+		if (!(scalarType instanceof GraphQLScalarType)) {
+			throw new Error(
+				`Type ${scalarType} cannot be validated. Only scalars are accepted`,
+			)
+		}
+	}
+}
